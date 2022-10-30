f076acdbd04ced71cf3b2fe06c8d44104f0f0360
gopixiedust <genjutsumaster10@gmail.com>
2022-10-30 22:42:30 +0530

    register page work

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ require('./db/mongoose')
 const express = require("express")
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const registerRouter = require('./router/register')
 const PORT = process.env.PORT
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://nipuntopno:Nipun123@cluster0.ydchfxl.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    // useFindAndModify:true,
+    useUnifiedTopology:true
+    // useCreateIndex:true
+})
 
 const app = express()
 app.set('view engine', 'ejs')
 
 //views folder
-app.set('views', path.join(__dirname,'../template'))
+app.set('views', path.join(__dirname,'../template/views'))
 
 //public folder
 app.use(express.static(path.join(__dirname, '../public')))
@@ -19,6 +28,8 @@ app.use(cookieParser())
 //body parser
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(PORT, () => {
-    console.log("Server is up!")
+app.use(registerRouter)
+
+app.listen(3000, () => {
+    console.log('Server is up!~')
 })
---
+++ src/router/register.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const User = require('../model/user.js')
+// const unauth = require('../middleware/unauth')
+
+const router = new express.Router()
+
+router.get('/register', (req, res) => {
+    res.render('register', {error: req.query.error, auth: req.query.auth})
+})
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        gender: req.body.gender,
+        post: req.body.designation,
+        leavesLeft: 0
+    })
+
+    try {
+        const token = await user.generateAuthToken()
+        res.cookie('auth_token', token)
+        res.redirect('/user')
+    }
+    catch (e) {
+        res.redirect('/register?error=1')
+    }
+})
+
+module.exports = router
